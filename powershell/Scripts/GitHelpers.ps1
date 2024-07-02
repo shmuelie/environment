ceac2638ad420f3dd19389e027ffcdc4a31ab8c0
Shmueli Englard <senglard@microsoft.com>
2024-07-02 10:14:23 -0700

    Fix for getting worktrees with branch data that's too short

--- powershell/Scripts/GitHelpers.ps1
+++ powershell/Scripts/GitHelpers.ps1
@@ -2,7 +2,9 @@ function Get-Worktrees() {
     ((git worktree list --porcelain) -join ',') -split ',,' | ConvertFrom-Csv -Header 'Path','Commit','Branch' | ForEach-Object {
         $_.Path = (Resolve-Path $_.Path.SubString(9))
         $_.Commit = $_.Commit.SubString(5)
-        $_.Branch = $_.Branch.SubString(18)
+        if ($_.Branch.Length -gt 18) {
+            $_.Branch = $_.Branch.SubString(18)
+        }
         $_
     }
 }
